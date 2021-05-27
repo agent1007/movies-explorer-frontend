3c6e3e52a559c77e4434251719e3402d0178789d
agent1007 <pavelcherkasov@mail.ru>
2021-05-27 18:04:21 +0300

    Diplom, stage #3

--- movies-explorer/src/components/SearchForm/SearchForm.js
+++ movies-explorer/src/components/SearchForm/SearchForm.js
@@ -4,11 +4,11 @@ import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 function SearchForm() {
     return (
         <div className="search-form">
-            <div className="search-form__bar">
-                <input className="search-form__bar-input" type="text" name='name-film' id="name-film-input" />
+            <form className="search-form__bar">
+                <input className="search-form__bar-input" type="text" name='name-film' id="name-film-input" required/>
                 <span id="name-film-input-error" className="error"/>
                 <button type="submit" className="search-form__bar-button"></button>   
-            </div>
+            </form>
             <div className="search-form__chapter">Короткометражки</div>
             <FilterCheckbox/>
         </div>
